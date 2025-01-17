c5d09cb6184779c22d78d319bca50d45a7c44ff0
islamsamy214 <islamsamy214@gmail.com>
2025-01-17 23:45:30 +0400

    fix: enforce unique constraint on username field in users table migration

--- database/migrations/0001_users_table.go
+++ database/migrations/0001_users_table.go
@@ -21,7 +21,7 @@ func (*UserTable) Up() {
 	_, err = db.Create(`
 		CREATE TABLE IF NOT EXISTS users (
 			id SERIAL PRIMARY KEY,
-			username TEXT NOT NULL,
+			username TEXT NOT NULL UNIQUE,
 			password TEXT NOT NULL,
 			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 			update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
