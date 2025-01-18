951ae7dcd368dc6b9c12409008627cd5455320cc
islamsamy214 <islamsamy214@gmail.com>
2025-01-18 14:47:31 +0400

    refactor: enhance event routes by applying authentication middleware

--- routes/http/apis/api.go
+++ routes/http/apis/api.go
@@ -2,24 +2,18 @@ package apis
 
 import (
 	"web-app/app/http/controllers"
+	"web-app/app/http/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Regester(route *gin.Engine) {
-	// auth
+	// authentication routes
 	authController := controllers.NewAuthController()
 	route.POST("/login", authController.Login)
 
-	// events
+	// events routes
 	eventController := controllers.NewEventController()
-	route.GET("/events", eventController.Index)
-	route.POST("/events", eventController.Create)
-
-	// // group it to middleware
-	// auth := route.Group("/events")
-	// auth.Use(middlewares.Authenticate)
-	// auth.POST("", eventController.Create)
-	// auth.PUT("/:id", eventController.Update)
-	// auth.DELETE("/:id", eventController.Delete)
+	route.GET("/events", middlewares.Authenticate, eventController.Index)
+	route.POST("/events", middlewares.Authenticate, eventController.Create)
 }
