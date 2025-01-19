1e16f9afc86349fa614b3ae790b86fe8af527da4
islamsamy214 <islamsamy214@gmail.com>
2025-01-19 13:39:56 +0400

    refactor: rename NewKafkaConfig to NewKafkaConsumerConfig and add NewProducerConfig for clarity

--- configs/kafka.go
+++ configs/kafka.go
@@ -6,11 +6,17 @@ import (
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
-func NewKafkaConfig() *kafka.ConfigMap {
+func NewKafkaConsumerConfig() *kafka.ConfigMap {
 	return &kafka.ConfigMap{
 		"bootstrap.servers":  os.Getenv("KAFKA_BROKERS"),
 		"group.id":           os.Getenv("KAFKA_CONSUMER_GROUP_ID"),
 		"auto.offset.reset":  os.Getenv("KAFKA_OFFSET_RESET"),
 		"enable.auto.commit": false, // Disable auto commit for manual offset management
+	}
+}
+
+func NewProducerConfig() *kafka.ConfigMap {
+	return &kafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("KAFKA_BROKERS"),
 	}
 }
