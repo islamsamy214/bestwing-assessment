79fe8c4a899086798bd2fb927fe201bedd532241
islamsamy214 <islamsamy214@gmail.com>
2025-01-20 13:09:48 +0400

    feat: enhance Kafka consumer and producer configurations with timeout settings

--- configs/kafka.go
+++ configs/kafka.go
@@ -8,15 +8,22 @@ import (
 
 func NewKafkaConsumerConfig() *kafka.ConfigMap {
 	return &kafka.ConfigMap{
-		"bootstrap.servers":  os.Getenv("KAFKA_BROKERS"),
-		"group.id":           os.Getenv("KAFKA_CONSUMER_GROUP_ID"),
-		"auto.offset.reset":  os.Getenv("KAFKA_OFFSET_RESET"),
-		"enable.auto.commit": false, // Disable auto commit for manual offset management
+		"bootstrap.servers":     os.Getenv("KAFKA_BROKERS"),
+		"group.id":              os.Getenv("KAFKA_CONSUMER_GROUP_ID"),
+		"auto.offset.reset":     os.Getenv("KAFKA_OFFSET_RESET"),
+		"enable.auto.commit":    false,
+		"session.timeout.ms":    30000,
+		"max.poll.interval.ms":  300000,
+		"heartbeat.interval.ms": 3000,
+		"socket.timeout.ms":     30000,
 	}
 }
 
 func NewKafkaProducerConfig() *kafka.ConfigMap {
 	return &kafka.ConfigMap{
-		"bootstrap.servers": os.Getenv("KAFKA_BROKERS"),
+		"bootstrap.servers":    os.Getenv("KAFKA_BROKERS"),
+		"socket.timeout.ms":    60000,
+		"session.timeout.ms":   60000,
+		"max.poll.interval.ms": 300000,
 	}
 }
